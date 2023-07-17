8467c1fa03c6cda9ee00443d0d21773552d5d845
KyleHS <k15156@yahoo.com>
2023-07-17 13:22:13 -0600

    added in some crud notes for restful

---
+++ index.js
@@ -0,0 +1,8 @@
+
+//-------------CRUD---------------
+
+//GET /comments - list all comments
+//POST /comments - create a new comment
+//GET /comments/:id -get one comment (using ID)
+//PATCH /comments/:id - update one comment
+//DELETE /comments/:id -destroy one comment
