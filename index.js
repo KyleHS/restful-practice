dbaa031974aa34730612597998a58427ec9766ed
KyleHS <k15156@yahoo.com>
2023-07-19 15:46:15 -0600

    fixed some errors in loading the show.ejs file when clicking on hyperlink.  Also added in notes for restful

--- index.js
+++ index.js
@@ -11,9 +11,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const { v4: uuid } = require('uuid');  //package that allows us to use unique ids for each comment in array
+const methodOverride = require('method-override');
+
 
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
+app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
@@ -44,49 +47,82 @@ const comments = [
     comment: 'Sigh... animal'
 }
 ]
+// **********************************
+// INDEX - renders multiple comments
+// **********************************
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments });
+})
+// **********************************
+// NEW - renders a form
+// **********************************
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new');
+})
+// **********************************
+// CREATE - creates a new comment
+// **********************************
+app.post('/comments', (req, res) => {
+    const { username, comment } = req.body;
+    comments.push({ username, comment, id: uuid() })
+    res.redirect('/comments');
+})
+// *******************************************
+// SHOW - details about one particular comment
+// *******************************************
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/show', { comment })
+})
+// *******************************************
+// EDIT - renders a form to edit a comment
+// *******************************************
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', { comment })
+})
+// *******************************************
+// UPDATE - updates a particular comment
+// *******************************************
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const foundComment = comments.find(c => c.id === id);
+
+    //get new text from req.body
+    const newCommentText = req.body.comment;
+    //update the comment with the data from req.body:
+    foundComment.comment = newCommentText;
+    //redirect back to index (or wherever you want)
+    res.redirect('/comments')
+})
+
+// *******************************************
+// DELETE/DESTROY- removes a single comment
+// *******************************************
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments');
+})
+
 app.get('/tacos', (req, res) => {
     res.send("GET /tacos response")
 })
 
 app.post('/tacos', (req, res) => {
     const { meat, qty } = req.body;
-    res.send(`Here are your ${qty} ${meat} tacos`)
-})
-
-app.get('/comments/:id', (req, res) => {
-    const { id } = req.params;
-    const comment = comments.find(c => c.id ===id)
-    res.render ('comments/show', { comment }) 
-})
-
-app.get('/comments', (req, res) => {
-    res.render('comments/index', { comments })
-})
-
-app.get('/comments/new', (req, res) => {
-    res.render('comments/new')
-})
-
-app.get('/comment/:id/edit', (req, res) => {
-    const { id } = req.params;
-    const comment = comments.find(c => c.id === id);
-    res.render('comments/edit', { comment })
-})
-
-app.post('/comments', (req, res) => {
-    const { username, comment } = req.body;
-    comments.push({ username, comment, id: uuid() })
-    res.redirect("/comments");  //sends the user back to /comments after submitting comment
-})
-
-app.patch('/comments/:id', (req, res) => {
-    const { id } = req.params;
-    const newCommentText = req.body.comment;
-    const foundComment = comments.find(c => c.id === id);
-    foundComment.comment = newCommentText;
-    res.redirect('/comments')
+    res.send(`OK, here are your ${qty} ${meat} tacos`)
 })
 
 app.listen(3000, () => {
-    console.log("on port 3000")
+    console.log("ON PORT 3000!")
 })
+
+
+// GET /comments - list all comments
+// POST /comments - Create a new comment 
+// GET /comments/:id - Get one comment (using ID)
+// PATCH /comments/:id - Update one comment
+// DELETE /comments/:id - Destroy one comment
