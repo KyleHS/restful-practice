2424fa9b202c5a8dba083b1f79987454514c260b
KyleHS <k15156@yahoo.com>
2023-07-19 15:28:52 -0600

    installed method-override and created a way for a user to edit the current comments that are posted

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const { v4: uuid }= require('uuid');  //package that allows us to use unique ids for each comment in array
+const { v4: uuid } = require('uuid');  //package that allows us to use unique ids for each comment in array
 
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
@@ -67,10 +67,24 @@ app.get('/comments/new', (req, res) => {
     res.render('comments/new')
 })
 
+app.get('/comment/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', { comment })
+})
+
 app.post('/comments', (req, res) => {
     const { username, comment } = req.body;
     comments.push({ username, comment, id: uuid() })
     res.redirect("/comments");  //sends the user back to /comments after submitting comment
+})
+
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const newCommentText = req.body.comment;
+    const foundComment = comments.find(c => c.id === id);
+    foundComment.comment = newCommentText;
+    res.redirect('/comments')
 })
 
 app.listen(3000, () => {
